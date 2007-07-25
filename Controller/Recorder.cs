a45d63cdc41d43fd6f0836e050372eb86624f29b
Alan McGovern <alan.mcgovern@gmail.com>
2007-07-25 22:57:07 +0000

    Added the Recorder class used for recording animations
    svn path=/trunk/lunareclipse/; revision=82707

---
+++ Controller/Recorder.cs
@@ -0,0 +1,17 @@
+// Recorder.cs created with MonoDevelop
+// User: alan at 7:14 PM 7/25/2007
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace LunarEclipse
+{
+	public class Recorder
+	{
+		public Recorder()
+		{
+		}
+	}
+}
