4783956de9f330ddd2d4f17f59a11b25bc0a234a
Alan McGovern <alan.mcgovern@gmail.com>
2007-08-23 18:28:59 +0000

    * Model/SelectedBorder.cs: Refactored the CreateCircle and SetCircle   methods. No more redundant creation of brushes * Properties/PropertyType.cs: Added point
    svn path=/trunk/lunareclipse/; revision=84731

--- Model/SelectedBorder.cs
+++ Model/SelectedBorder.cs
@@ -164,11 +164,23 @@ namespace LunarEclipse.Model
 
             DrawHandles(((VisualCollection)base.GetValue(ChildrenProperty)).Count == 0);
         }
+		
+		private Ellipse CreateCircle()
+		{
+			Ellipse circle = new Ellipse();
+			circle.Width = HandleRadius * 2;
+			circle.Height = HandleRadius * 2;
+			circle.Fill = new SolidColorBrush(Colors.White);
+			circle.Stroke = new SolidColorBrush(Colors.Blue);
+			circle.Cursor = System.Windows.Input.Cursors.Hand;
+			Children.Add(circle);
+			
+			return circle;
+		}
         
         private void DrawHandles(bool firstDraw)
         {
             VisualCollection Children = (VisualCollection)base.GetValue(ChildrenProperty);
-            Ellipse circle;
             
             double left = 0;
             double top = 0;
@@ -180,135 +192,107 @@ namespace LunarEclipse.Model
             // Top left rotate handle
             if(firstDraw)
             {
-                circle = new Ellipse();
-                Children.Add(circle);
-                rotate1 = circle;
-                circle.MouseLeftButtonDown += delegate { Handle = rotate1; Console.WriteLine("Rotate1 {0}", rotate1); };
-                
-                
-                circle = new Ellipse();
-                Children.Add(circle);
-                height1= circle;
-                circle.MouseLeftButtonDown+= delegate { Handle = height1; Console.WriteLine("height1 {0}", height1);};
-                
-                
-                circle = new Ellipse();
-                Children.Add(circle);
-                rotate2 = circle;
-                circle.MouseLeftButtonDown+= delegate { Handle = rotate2; };
-                
-                
-                circle = new Ellipse();
-                Children.Add(circle);
-                width1 = circle;
-                circle.MouseLeftButtonDown += delegate { Handle = width1; };
-                
-                
-                circle = new Ellipse();
-                Children.Add(circle);
-                rotate3 = circle;
-                circle.MouseLeftButtonDown += delegate { Handle = rotate3; };
-                
-                
-                circle = new Ellipse();
-                Children.Add(circle);
-                height2 = circle;
-                circle.MouseLeftButtonDown += delegate { Handle = height2; };
-                
-                
-                circle = new Ellipse();
-                Children.Add(circle);
-                rotate4 = circle;
-                circle.MouseLeftButtonDown+= delegate { Handle = rotate4; };
-                
-                
-                circle = new Ellipse();
-                Children.Add(circle);
-                width2 = circle;
-                circle.MouseLeftButtonDown+= delegate { Handle = width2; };
-                
-                lines = new Shape[8];
-                for(int i=0; i < 8; i++)
-                {
-                    lines[i] = new Line();
-                    Children.Add(lines[i]);
-                }
-            }
-            
-            // Top left rotate handle
-            SetCircle(rotate1, new Point(left, top));
-            // Top centre stretch handle
-            SetCircle(height1, new Point(midX - HandleRadius, top));
-            // Top right rotate handle
-            SetCircle(rotate2, new Point(right - HandleRadius * 2, top));
-            // Middle right stretch handle
-            SetCircle(width1, new Point(right - HandleRadius * 2, midY - HandleRadius));
-            // Bottom right rotate handle
-            SetCircle(rotate3, new Point(right - HandleRadius * 2, bottom - HandleRadius * 2));
-            // Bottom centre stretch handle
-            SetCircle(height2, new Point(midX - HandleRadius, bottom - HandleRadius * 2));
-            // Bottom left rotate handle
-            SetCircle(rotate4, new Point(left, bottom - HandleRadius * 2));
-            // Middle left stretch handle
-            SetCircle(width2, new Point(left, midY - HandleRadius));
-            
-            // Top left line
-            SetLine(lines[0], new Point(left + HandleRadius * 2, top + HandleRadius),
-                              new Point(midX - HandleRadius, top + HandleRadius));
-           
-           
-            // Top right line
-            SetLine(lines[1], new Point(midX + HandleRadius, top + HandleRadius),
-                              new Point(right - HandleRadius * 2, top + HandleRadius));
-           
-            
-            // Right top line
-            SetLine(lines[2], new Point(right - HandleRadius, top + HandleRadius * 2),
-                              new Point(right - HandleRadius, midY - HandleRadius));
-           
-            // Right bottom line
-            SetLine(lines[3], new Point(right - HandleRadius, midY + HandleRadius),
-                              new Point(right - HandleRadius, bottom - HandleRadius * 2));
-           
-            
-            // Bottom right line
-            SetLine(lines[4], new Point(right - HandleRadius * 2, bottom - HandleRadius),
-                              new Point(midX + HandleRadius, bottom - HandleRadius));
-           
-           
-            // Bottom left line
-            SetLine(lines[5], new Point(midX - HandleRadius, bottom - HandleRadius),
-                              new Point(left + HandleRadius * 2, bottom - HandleRadius));
-           
-           
-            // Left bottom line
-            SetLine(lines[6], new Point(left + HandleRadius, bottom - HandleRadius * 2),
-                              new Point(left + HandleRadius, midY + HandleRadius));
-           
-            // Left top line
-            SetLine(lines[7], new Point(left + HandleRadius, midY - HandleRadius),
-                              new Point(left + HandleRadius, top + HandleRadius * 2));
-        }
-        
-        private void SetLine(Shape shape, Point start, Point end)
-        {
+				rotate1 = CreateCircle();
+				rotate1.MouseLeftButtonDown += delegate { Handle = rotate1; Console.WriteLine("Rotate1 {0}", rotate1); };
+				
+				rotate2 = CreateCircle();
+				rotate2.MouseLeftButtonDown+= delegate { Handle = rotate2; };
+				
+				rotate3 = CreateCircle();
+				rotate3.MouseLeftButtonDown += delegate { Handle = rotate3; };
+				
+				rotate4 = CreateCircle();
+				rotate4.MouseLeftButtonDown+= delegate { Handle = rotate4; };
+				
+				width1 = CreateCircle();
+				width1.MouseLeftButtonDown += delegate { Handle = width1; };
+				
+				width2 = CreateCircle();
+				width2.MouseLeftButtonDown+= delegate { Handle = width2; };
+				
+				height1 = CreateCircle();
+				height1.MouseLeftButtonDown+= delegate { Handle = height1; Console.WriteLine("height1 {0}", height1);};
+				
+				height2 = CreateCircle();
+				height2.MouseLeftButtonDown += delegate { Handle = height2; };
+				
+				lines = new Shape[8];
+				for(int i=0; i < lines.Length; i++)
+				{
+					lines[i] = new Line();
+					lines[i].StrokeThickness = 2;
+					lines[i].Stroke = new SolidColorBrush(Colors.Black);
+					Children.Add(lines[i]);
+				}
+			}
+			
+			// Top left rotate handle
+			SetCircle(rotate1, new Point(left, top));
+			// Top centre stretch handle
+			SetCircle(height1, new Point(midX - HandleRadius, top));
+			// Top right rotate handle
+			SetCircle(rotate2, new Point(right - HandleRadius * 2, top));
+			// Middle right stretch handle
+			SetCircle(width1, new Point(right - HandleRadius * 2, midY - HandleRadius));
+			// Bottom right rotate handle
+			SetCircle(rotate3, new Point(right - HandleRadius * 2, bottom - HandleRadius * 2));
+			// Bottom centre stretch handle
+			SetCircle(height2, new Point(midX - HandleRadius, bottom - HandleRadius * 2));
+			// Bottom left rotate handle
+			SetCircle(rotate4, new Point(left, bottom - HandleRadius * 2));
+			// Middle left stretch handle
+			SetCircle(width2, new Point(left, midY - HandleRadius));
+			
+			// Top left line
+			SetLine(lines[0], new Point(left + HandleRadius * 2, top + HandleRadius),
+			        new Point(midX - HandleRadius, top + HandleRadius));
+			
+			
+			// Top right line
+			SetLine(lines[1], new Point(midX + HandleRadius, top + HandleRadius),
+			        new Point(right - HandleRadius * 2, top + HandleRadius));
+			
+			
+			// Right top line
+			SetLine(lines[2], new Point(right - HandleRadius, top + HandleRadius * 2),
+			        new Point(right - HandleRadius, midY - HandleRadius));
+			
+			// Right bottom line
+			SetLine(lines[3], new Point(right - HandleRadius, midY + HandleRadius),
+			        new Point(right - HandleRadius, bottom - HandleRadius * 2));
+			
+			
+			// Bottom right line
+			SetLine(lines[4], new Point(right - HandleRadius * 2, bottom - HandleRadius),
+			        new Point(midX + HandleRadius, bottom - HandleRadius));
+			
+			
+			// Bottom left line
+			SetLine(lines[5], new Point(midX - HandleRadius, bottom - HandleRadius),
+			        new Point(left + HandleRadius * 2, bottom - HandleRadius));
+			
+			
+			// Left bottom line
+			SetLine(lines[6], new Point(left + HandleRadius, bottom - HandleRadius * 2),
+			        new Point(left + HandleRadius, midY + HandleRadius));
+			
+			// Left top line
+			SetLine(lines[7], new Point(left + HandleRadius, midY - HandleRadius),
+			        new Point(left + HandleRadius, top + HandleRadius * 2));
+		}
+		
+		private void SetLine(Shape shape, Point start, Point end)
+		{
             Line line = (Line)shape;
-            line.X1 = start.X;  line.Y1 = start.Y;
-            line.X2 = end.X;    line.Y2 = end.Y;
-            line.StrokeThickness = 2;
-            line.Stroke = new SolidColorBrush(Colors.Black);
-        }
-        
-        private void SetCircle(Shape shape, Point start)
-        {
-            Ellipse circle = (Ellipse)shape;
-            circle.SetValue<double>(TopProperty, start.Y);
-            circle.SetValue<double>(LeftProperty, start.X);
-            circle.Width = HandleRadius * 2;
-            circle.Height = HandleRadius * 2;
-            circle.Fill = new SolidColorBrush(Colors.White);
-            circle.Stroke = new SolidColorBrush(Colors.Blue);
-            circle.Cursor = System.Windows.Input.Cursors.Hand;
-        }
+			line.X1 = start.X;  line.Y1 = start.Y;
+			line.X2 = end.X;    line.Y2 = end.Y;
+		}
+		
+		private void SetCircle(Shape shape, Point start)
+		{
+			Ellipse circle = (Ellipse)shape;
+			circle.SetValue<double>(TopProperty, start.Y);
+			circle.SetValue<double>(LeftProperty, start.X);
+		}
     }
 }
--- Properties/PropertyType.cs
+++ Properties/PropertyType.cs
@@ -21,6 +21,7 @@ namespace LunarEclipse
 		PenLineCap,
 		PenLineJoin,
 		Visibility,
-		Point
+		Point,
+		Brush
 	}
 }
